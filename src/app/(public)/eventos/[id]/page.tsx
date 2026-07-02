d40da1fc71e4c55d5a6c400491a31e25f6c6d036
JONATHAN JOSUE CEPEDA AYALA <jjcepedaa@utn.edu.ec>
2026-07-01 22:07:37 -0500

    cambios funcionales

--- src/app/(public)/eventos/[id]/page.tsx
+++ src/app/(public)/eventos/[id]/page.tsx
@@ -34,7 +34,7 @@ export default async function EventPage({ params }: EventPageProps) {
     weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'
   })
   
-  const esInscripcionAbierta = actividad.fecha_limite_inscripcion && new Date() <= new Date(actividad.fecha_limite_inscripcion)
+  const esInscripcionAbierta = actividad.fecha_fin && new Date() <= new Date(actividad.fecha_fin)
   
   return (
     <div className={styles.pageWrapper}>
@@ -80,11 +80,11 @@ export default async function EventPage({ params }: EventPageProps) {
               <li>
                 <strong>Total Inscritos:</strong> {inscritosCount || 0}
               </li>
-              {actividad.fecha_limite_inscripcion ? (
+              {actividad.fecha_fin ? (
                 <li>
                   <strong>Cierre de inscripción:</strong><br />
                   <span className={esInscripcionAbierta ? styles.openText : styles.closedText}>
-                    {new Date(actividad.fecha_limite_inscripcion).toLocaleDateString('es-EC', {
+                    {new Date(actividad.fecha_fin).toLocaleDateString('es-EC', {
                       day: 'numeric', month: 'long', year: 'numeric', hour: '2-digit', minute:'2-digit'
                     })}
                   </span>
@@ -102,7 +102,7 @@ export default async function EventPage({ params }: EventPageProps) {
 
           {/* Formulario de inscripción */}
           <div className={styles.formWrapper}>
-            {esInscripcionAbierta || !actividad.fecha_limite_inscripcion ? (
+            {esInscripcionAbierta || !actividad.fecha_fin ? (
               <EnrollForm idActividad={actividad.id_actividad} />
             ) : (
               <div className={styles.closedMessage}>
